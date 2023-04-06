cd29a567857343d16f596907affef2c771eae4d1
Leo Almeida <leowoa@outlook.com>
2023-04-06 11:24:32 -0300

    made another design and some data structure

--- script.js
+++ script.js
@@ -7,53 +7,58 @@ $(".calcular").on("click",function (){
     let arrival = parseInt($(".arrival").val().replaceAll(".", ""));
     let ground = parseInt($(".ground").val().replaceAll(".", ""));
 
-    const aRow = $(".a-row");
-    const bRow = $(".b-row");
-    const cRow = $(".c-row");
-    const dRow = $(".d-row");
-    const eRow = $(".e-row");
-    const fRow = $(".f-row");
-    const gRow = $(".g-row");
-    const hRow = $(".h-row");
-    const iRow = $(".i-row");
-    const jRow = $(".j-row");
+    if(isNaN(solicitado) || isNaN(nota1) || isNaN(nota2) || isNaN(gravidade) || isNaN(arrival) || isNaN(ground)){
+        alert("Faltou preencher algum campo");
+    } else {
+        const aRow = $(".a-row");
+        const bRow = $(".b-row");
+        const cRow = $(".c-row");
+        const dRow = $(".d-row");
+        const eRow = $(".e-row");
+        const fRow = $(".f-row");
+        const gRow = $(".g-row");
+        const hRow = $(".h-row");
+        const iRow = $(".i-row");
+        const jRow = $(".j-row");
 
-    let finalResultA = numberToArray(solicitado)
-    let finalResultB = numberToArray((arrival - ground));
-    let finalResultC = numberToArray((solicitado - (arrival - ground)));
-    let finalResultD = numberToArray((nota1 + nota2));
-    let finalResultF = numberToArray(arrival);
-    let finalResultG = numberToArray((nota1 + nota2) * gravidade);
-    let finalResultH = numberToArray(solicitado);
-    let finalResultI = numberToArray(ground);
-    let finalResultJ = numberToArray(Math.round((((nota1 + nota2) * gravidade) - (solicitado - arrival))- ground));
+        let finalResultA = numberToArray(Math.round(solicitado));
+        let finalResultB = numberToArray(Math.round((arrival - ground)));
+        let finalResultC = numberToArray(Math.round((solicitado - (arrival - ground))));
+        let finalResultD = numberToArray(Math.round((nota1 + nota2)));
+        let finalResultF = numberToArray(Math.round(arrival));
+        let finalResultG = numberToArray(Math.round((nota1 + nota2) * gravidade));
+        let finalResultH = numberToArray(Math.round(solicitado));
+        let finalResultI = numberToArray(Math.round(ground));
+        let finalResultJ = numberToArray(Math.round((((nota1 + nota2) * gravidade) - (solicitado - arrival))- ground));
+        
 
-    console.log(finalResultJ);
+        // solicitado = 119900
+        //nota 1 = 69909
+        //nota 2 = 69910
+        //gravidade = 0,796
+        //arrival = 10400
+        //used on ground = 1000
 
-    // solicitado = 119900
-    //nota 1 = 69909
-    //nota 2 = 69910
-    //gravidade = 0,796
-    //arrival = 10400
-    //used on ground = 1000
+        showInScreen(finalResultA, aRow);
+        showInScreen(finalResultB, bRow);
+        showInScreen(finalResultC, cRow);
+        showInScreen(finalResultD, dRow);
+        showInScreen(finalResultF, fRow);
+        showInScreen(finalResultG, gRow);
+        showInScreen(finalResultH, hRow);
+        showInScreen(finalResultI, iRow);
+        showInScreen(finalResultJ, jRow);
 
-    showInScreen(finalResultA, aRow);
-    showInScreen(finalResultB, bRow);
-    showInScreen(finalResultC, cRow);
-    showInScreen(finalResultD, dRow);
-    showInScreen(finalResultF, fRow);
-    showInScreen(finalResultG, gRow);
-    showInScreen(finalResultH, hRow);
-    showInScreen(finalResultI, iRow);
-    showInScreen(finalResultJ, jRow);
+        let toGravity = [];
+        let digits = $(".gravidade").val().replace(",",".").split('');
+        for (let i = 0; i < digits.length; i++) {
+            toGravity.push(digits[i]);
+        }
 
-    let toGravity = [];
-    let digits = $(".gravidade").val().replace(",",".").split('');
-    for (let i = 0; i < digits.length; i++) {
-        toGravity.push(digits[i]);
+        showInScreen(toGravity, eRow);
     }
 
-    showInScreen(toGravity, eRow);
+
 
 
 
